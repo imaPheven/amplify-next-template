18a1fa73d75f43975d6da156f6200c30737f1a6b
imaCoden <wobbler05_needle@icloud.com>
2025-02-15 18:52:06 -0700

    Updated so layout to be client rendered

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,16 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-import { Authenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export default function App() {
+    
+  const { signOut } = useAuthenticator();
+  
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -36,10 +38,9 @@ export default function App() {
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
   }
-
+  
   return (
     <main>
-      <Authenticator>
         <h1>My todos</h1>
         <button onClick={createTodo}>+ new</button>
         <ul>
@@ -56,7 +57,7 @@ export default function App() {
             Review next steps of this tutorial.
           </a>
         </div>
-      </Authenticator>
+        <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
