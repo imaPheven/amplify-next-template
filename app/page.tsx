4aac71249573291f682a96c9d7665e4716ed85c1
imaCoden <wobbler05_needle@icloud.com>
2025-02-15 19:15:29 -0700

    Fixes authenticator

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-import { Authenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
-
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export default function App() {
+    
+  const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -39,7 +39,6 @@ export default function App() {
 
   return (
     <main>
-      <Authenticator>
         <h1>My todos</h1>
         <button onClick={createTodo}>+ new</button>
         <ul>
@@ -56,7 +55,7 @@ export default function App() {
             Review next steps of this tutorial.
           </a>
         </div>
-      </Authenticator>
+        <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
